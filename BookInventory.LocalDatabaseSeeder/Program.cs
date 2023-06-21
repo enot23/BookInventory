d79c0cf36628e9c92c023620d9b14ffb705f8a96
Bohdan_Maryniak <bohdan.maryniak@bitsorchestra.com>
2023-06-22 02:31:25 +0300

    Add LocalDb Seeder

---
+++ BookInventory.LocalDatabaseSeeder/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BookInventory.LocalDatabaseSeeder;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = config.GetConnectionString("AppConnectionString");
+            
+        var dbContextOptions = new DbContextOptionsBuilder<Data.AppContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+			
+        var context = new Data.AppContext(dbContextOptions);
+
+        Console.WriteLine(@"Dropping Database...");
+        context.Database.EnsureDeleted(); // Deletes database if exists  
+			
+        Console.WriteLine(@"Creating Database structure...");
+        context.Database.Migrate(); // Creates database and structure
+    }
+}
